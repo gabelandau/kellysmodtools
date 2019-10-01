98eb27c54c0e53c4025e0285649a5dad3c0f5a99
gabelandau <me@gabelandau.com>
2019-09-30 20:28:48 -0400

    Started backend configuration

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 DEBUG = True
 
-app = Flask(__name__)
-app.config.from_object(__name__)
+app = Flask(__name__, static_folder = "../dist/static", template_folder = "../dist/templates")
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-  return jsonify('pong!')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index():
+  return render_template("index.html")
 
 if __name__ == '__main__':
   app.run()
